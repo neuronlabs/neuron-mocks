40fb3b7a84a35231dd955fc61562ded7fe914f3d
Jacek Kucharczyk <kucjac@gmail.com>
2020-04-09 19:43:29 +0200

    Updated neuron-core to v0.12.0.

--- factory.go
+++ factory.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/mock"
 
-	"github.com/neuronlabs/neuron-core/mapping"
+	"github.com/neuronlabs/neuron-core/config"
 	"github.com/neuronlabs/neuron-core/repository"
 )
 
@@ -15,6 +15,9 @@ Factory
 
 */
 
+// compile time check for the Factory interface implementation.
+var _ repository.Factory = &Factory{}
+
 // Factory is the repository.Factory mock implementation
 type Factory struct {
 	mock.Mock
@@ -22,7 +25,7 @@ type Factory struct {
 
 // New creates new repository
 // Implements repository.Factory method
-func (f *Factory) New(s repository.Controller, model *mapping.ModelStruct) (repository.Repository, error) {
+func (f *Factory) New(s repository.Controller, model *config.Repository) (repository.Repository, error) {
 	return &Repository{}, nil
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/neuronlabs/neuron-mocks
 go 1.13
 
 require (
-	github.com/neuronlabs/neuron-core v0.11.20
+	github.com/neuronlabs/neuron-core v0.12.0
 	github.com/stretchr/testify v1.4.0
 )
